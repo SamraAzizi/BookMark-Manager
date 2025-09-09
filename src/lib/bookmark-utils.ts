8670837b6f0be68a0a81b66821ecf4aec70923cc
SamraAzizi <samarraazizi@gmail.com>
2025-09-09 19:37:10 +0500

    book mark manager with next.js and clerk prisma

--- src/lib/bookmark-utils.ts
+++ src/lib/bookmark-utils.ts
@@ -10,6 +10,9 @@ export async function getUserBookmarks(userId: string):Promise<any>{
             where: {
                 userId,
             },
+            orderBy: {
+                createdAt: "desc",
+            },
 
     
     
@@ -17,5 +20,28 @@ export async function getUserBookmarks(userId: string):Promise<any>{
     return bookmarks;
 }catch(error){
     console.error("Error fetching bookmark: ", error)
+    throw new Error("Failed to fetch bookmarks")
 }
+}
+
+export async function createUserBookmark(userId: string, data: CreateBookmarkData): Promise<any>{
+    try{
+        if(!data.url|| !data.title){
+            throw new Error("URL and title are required");
+        }
+        const bookmark = await prisma.bookmark.create({
+            data: {
+                url: data.url,
+                title: data.title,
+                notes: data.notes || "",
+                userId
+            }
+        })
+        return bookmark;
+
+    }
+    catch(error){
+        console.error("Error creating bookmark: ", error);
+        throw new Error("Failed to create bookmark");
+    }
 }
