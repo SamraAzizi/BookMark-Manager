9493450b9e247711f6825a4c6cb10b48f526db2b
SamraAzizi <samarraazizi@gmail.com>
2025-09-10 16:41:43 +0500

    book mark manager with next.js and clerk prisma

--- src/app/api/bookmarks/route.ts
+++ src/app/api/bookmarks/route.ts
@@ -10,10 +10,37 @@ export async function GET(){
 
         }
         const bookmarks = await getUserBookmarks(userId);
-        return NextResponse
+        return NextResponse.json(bookmarks, {status:200});
     }
     catch(error){
         console.error("error fetching bookmarks: ", error);
-        return NextResponse.json({error: "Internal server error"}, {status:500});
+        return NextResponse.json({error: "failed to fetch bookmarks"}, {status:500});
     }
+}
+
+
+export async function POST(request:NextRequest){
+    try{
+        const {userId} = await auth();
+        if (!userId){
+            return NextResponse.json({error: "Unauthorized"}, {status:401});
+
+        }
+
+        const body = await request.json()
+        const {url, title, notes} = body;
+
+        if (!url || !title){
+            return NextResponse.json({error: "URL and title are required"}, {status:400})
+        }
+
+        const bookmark = await createUserBookmark(userId, {url, title, notes});
+        return NextResponse.json(bookmark, {status:201});
+        
+    }
+    catch(error){
+        console.error("error creating bookmarks: ", error);
+        return NextResponse.json({error: "failed to create bookmarks"}, {status:500});
+    }
+
 }
