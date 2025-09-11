6e0c181c35ccc543c8f8dd67626ad7c6e41d2276
SamraAzizi <samarraazizi@gmail.com>
2025-09-11 19:15:36 +0500

    book mark manager with next.js and clerk(global css)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body>
+        <header className="header">
+          <div>
+            <h1 style={{fontSize: "24px", fontWeight: "bold"}}>Bookmark Manager</h1>
+          </div>
+          <div style={{display: "flex", gap: "15px", alignContent: "center"}}>
+            <SignedOut>
+              <SignInButton>
+                <button className="btn btn-primary">Sign In</button>
+              </SignInButton>
+              <SignUpButton>
+                <button className="btn btn-primary">Sign Up</button>
+              </SignUpButton>
+            </SignedOut>
+          </div>
+        </header>
       </body>
     </html>
   );
