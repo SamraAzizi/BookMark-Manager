73a42797ad710c3db53366c13f44b4973917afee
SamraAzizi <samarraazizi@gmail.com>
2025-09-12 20:10:02 +0500

    book mark manager with next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
+"use client"
 
+import { use, useState } from "react";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { useBookmarks } from "@/hooks/useBookmarks";
+import { CreateBookmarkData } from "@/types/bookmark";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
- 
-  );
+
+  const { bookmarks, loading, error, addBookmark, deleteBookmark, refetch} = 
+    useBookmarks();
+
+
+
+    const [ showAddForm, setShowAddForm] = useState(false)
+    const [ isSubmitting, setIsSubmitting] = useState(false)
+
+    const handleAddBookmark = async (data: CreateBookmarkData) => {
+      try{
+        setIsSubmitting(true)
+        await addBookmark(data)
+      }
+    }
+
+
+
+
+
+  
 }
