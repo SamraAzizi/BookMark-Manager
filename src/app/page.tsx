d2857d8c560d149eee1598a647eb98dd77d1dcb5
SamraAzizi <samarraazizi@gmail.com>
2025-09-12 20:22:28 +0500

    book mark manager with next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,10 +20,51 @@ export default function Home() {
       try{
         setIsSubmitting(true)
         await addBookmark(data)
+        setShowAddForm(false)
+      }catch(err){
+        console.log("failed to add bookmark", err)
+      }finally{
+        setIsSubmitting(false)
+      }
+    }
+
+    const handleDeleteBookmark = async (id: string) =>{
+      if (confirm("Are you sure you want to delete this bookmark? ")){
+        try{
+          await deleteBookmark(id)
+        }catch(error){
+          console.log("Failed to delete bookmark", error)
+        }
       }
     }
 
 
+    if (loading){
+      return <div className={styles["loading-container"]}>
+        <div className={styles["loading-content"]}>
+          <p>Loading bookmark...</p>
+
+        </div>
+
+      </div>
+    }
+
+    return <div>
+      <SignedIn>
+
+
+      </SignedIn>
+
+      <SignedOut>
+        <div className={styles["Signedout-container"]}>
+          <div className={styles["Signout-content"]}>
+            <p className={styles["signout-text"]}>Please sign in to manage your bookmark</p>
+
+          </div>
+
+        </div>
+      </SignedOut>
+    </div>
 
 
 
