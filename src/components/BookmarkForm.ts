5021c33bbb1021d87bc8d3c1e066c51f2b07e3c9
SamraAzizi <samarraazizi@gmail.com>
2025-09-13 16:39:28 +0500

    book mark manager with next.js

--- src/components/BookmarkForm.ts
+++ src/components/BookmarkForm.ts
@@ -17,4 +17,23 @@ export default function BookmarkForm({
     isEditing = false,
     isSubmitting = false,
 
-})
+}: BookmarkFormProps){
+    const [ formData, setFormData] = useState<CreateBookmarkData>({
+        url: intialData?.url || "",
+        title: intialData?.title || "",
+        notes: intialData?.notes || "",
+    })
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (!formData.url.trim() || !formData.title.trim() || isSubmitting){
+            return
+        }
+        onSubmit(formData)
+
+        if(!isEditing){
+            setFormData({})
+        }
+
+    }
+}
