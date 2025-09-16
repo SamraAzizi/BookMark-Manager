6d5cfab341144f82fdb6d5ef6438d8fb64e3b1de
SamraAzizi <samarraazizi@gmail.com>
2025-09-16 16:11:28 +0500

    book mark manager with next.js

--- src/app/[transport]/route.ts
+++ src/app/[transport]/route.ts
@@ -1,21 +1,32 @@
 import { verifyClerkToken } from '@clerk/mcp-tools/next'
 import { createMcpHandler, experimental_withMcpAuth as withMcpAuth } from '@vercel/mcp-adapter'
 import { auth, clerkClient } from '@clerk/nextjs/server'
+import { getUserBookmarks, createUserBookmark } from '@/lib/bookmark-utils'
+import { CreateBookmarkData } from '@/types/bookmark'
+import { z } from "zod"
+
 
 const clerk = await clerkClient()
 
 const handler = createMcpHandler((server) => {
   server.tool(
-    'get-clerk-user-data',
-    'Gets data about the Clerk user that authorized this request',
+    'get-user-bookmarks',
+    'Gets all bookmarks for the authenticated users',
     {},
     async (_, { authInfo }) => {
-      const userId = authInfo!.extra!.userId! as string
-      const userData = await clerk.users.getUser(userId)
+      try{
 
-      return {
-        content: [{ type: 'text', text: JSON.stringify(userData) }],
+        const userId = authInfo!.extra!.userId! as string
+        const bookmarks = await getUserBookmarks(userId)
+        return {
+          content: [{type: "text", text: JSON.stringify(bookmarks)}]
+        }
+      
+      }catch(error){
+        return
       }
+
+      
     },
   )
 })
